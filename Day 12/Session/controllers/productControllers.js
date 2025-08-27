9295660b87d28bc4bd8772d897a1c07715c7d3a7
Mohamed Sayed <97993670+ShadowVoid-King@users.noreply.github.com>
2025-08-27 20:39:00 +0300

    Day 12 - Task 1 ( Finish 6 endpoints - Done Debug )

--- Day 12/Session/controllers/productControllers.js
+++ Day 12/Session/controllers/productControllers.js
@@ -15,17 +15,16 @@ async function isAdminUser(username) {
 	return currentUser && currentUser.role === "admin";
 }
 
-getUserToken = tokenData.findOne({ username });
-
 const productsGet = async (req, res) => {
 	try {
-		const user = await isLoggedIn(getUserToken);
-		if (user) {
-			const products = await productData.find();
-			return res.json(products);
-		} else {
-			res.status(401).send("401, User Not Logged In");
-		}
+		// const { username } = req.body;
+		// const user = await isLoggedIn(username);
+		// if (user) {
+		const products = await productData.find();
+		return res.json(products);
+		// } else {
+		// 	res.status(401).send("401, User Not Logged In");
+		// }
 	} catch (error) {
 		return res.json({ message: error.message });
 	}
@@ -33,12 +32,13 @@ const productsGet = async (req, res) => {
 
 const productGetById = async (req, res) => {
 	try {
-		const user = await isLoggedIn(getUserToken);
-		if (user) {
-			return res.json(await productData.findById(req.params.id));
-		} else {
-			res.status(401).send("401, User Not Logged In");
-		}
+		// const { username } = req.body;
+		// const user = await isLoggedIn(username);
+		// if (user) {
+		return res.json(await productData.findOne({ id: req.params.id }));
+		// } else {
+		// 	res.status(401).send("401, User Not Logged In");
+		// }
 	} catch (error) {
 		return res.json({ message: error.message });
 	}
@@ -46,7 +46,8 @@ const productGetById = async (req, res) => {
 
 const addProductPost = async (req, res) => {
 	try {
-		const user = await isLoggedIn(getUserToken);
+		const { username } = req.body;
+		const user = await isLoggedIn(username);
 		if (user) {
 			const { title, price, description, quantity } = req.body;
 			const products = await productData.find();
@@ -70,9 +71,10 @@ const addProductPost = async (req, res) => {
 
 const deleteProductDelete = async (req, res) => {
 	try {
-		const user = await isAdminUser(getUserToken);
+		const { username } = req.body;
+		const user = await isAdminUser(username);
 		if (user) {
-			await productData.findByIdAndDelete(req.params.id);
+			await productData.findOneAndDelete({ id: req.params.id });
 			res.status(200).json({ message: "Product deleted successfully" });
 		} else {
 			res.status(401).send("401, User Not Logged In");
@@ -84,10 +86,11 @@ const deleteProductDelete = async (req, res) => {
 
 const editProductPut = async (req, res) => {
 	try {
-		const user = await isAdminUser(getUserToken);
+		const { username } = req.body;
+		const user = await isAdminUser(username);
 		if (user) {
 			const { title, price, description, quantity } = req.body;
-			await productData.findByIdAndUpdate(req.params.id, {
+			await productData.findOneAndUpdate({ id: req.params.id }, {
 				title,
 				quantity,
 				price,
